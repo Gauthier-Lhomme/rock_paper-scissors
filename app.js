9dfccc5fc4c371bccb78afd0937cf044eefa7a5e
Gauthier <lhomme.gaut@gmail.com>
2022-03-27 17:04:09 +0200

    Create HTML structure and click event on User Choice

---
+++ app.js
@@ -0,0 +1,11 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>{
+   userChoice = e.target.id
+   userChoiceDisplay.innerHTML = userChoice
+}))
