a92c52fce800d486c7e1b2a90ec537bab94ae4bd
Gauthier <lhomme.gaut@gmail.com>
2022-03-27 17:11:04 +0200

    Create computer's choice function

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ let userChoice
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) =>{
    userChoice = e.target.id
    userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) +1 // Le résultat sera 1, 2 ou 3
+}
